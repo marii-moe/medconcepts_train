dbd51a3b3cc221499ce6b8df122dcad1b77a35a9
marii <marii@marii.moe>
2026-03-14 20:52:19 +1100

    updated test

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 from medconcepts_train import load_environment
+import verifiers as vf
 
-env = load_environment()
-dataset = env.dataset
+env_id = 'medconcepts_train'
 
-for row in dataset.select(range(5)):
+env = vf.load_environment(env_id)
+
+print(env)
+
+train_env_group = vf.EnvGroup(
+    envs=[env],
+    env_names=[env_id],
+    map_kwargs=dict(writer_batch_size=1),  # set defensively to not error on map operations on large datasets
+)
+
+train_dataset = train_env_group.get_dataset()
+
+for row in train_dataset.select(range(5)):
     print(row)
     print()
